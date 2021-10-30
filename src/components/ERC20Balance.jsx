56cce4e4aabf46db041dc334a9f8908187122b34
topde199725 <topdev0205@gmail.com>
2021-10-30 23:10:35 +0200

    feat: added maxWidth for <ERC20Balance>

--- src/components/ERC20Balance.jsx
+++ src/components/ERC20Balance.jsx
@@ -3,24 +3,29 @@ import { useMoralis } from "react-moralis";
 import { useERC20Balance } from "../hooks/useERC20Balance";
 import { Skeleton, Table, Image } from "antd";
 import { getEllipsisTxt } from "../utils/formatters";
-
 const styles = {
   title: {
     fontSize: "30px",
     fontWeight: "700",
-  }
+  },
 };
-
 function ERC20Balance(props) {
   const { assets } = useERC20Balance(props);
   const { Moralis } = useMoralis();
-  
+
   const columns = [
     {
       title: "",
       dataIndex: "logo",
       key: "logo",
-      render: (logo) => <Image src={logo} alt="https://etherscan.io/images/main/empty-token.png" width="30px" height="30px"/>,
+      render: (logo) => (
+        <Image
+          src={logo}
+          alt="https://etherscan.io/images/main/empty-token.png"
+          width="30px"
+          height="30px"
+        />
+      ),
     },
     {
       title: "Name",
@@ -44,28 +49,23 @@ function ERC20Balance(props) {
       title: "Address",
       dataIndex: "token_address",
       key: "token_address",
-      render: (address) => (
-          getEllipsisTxt(address, 5)
-        ),
+      render: (address) => getEllipsisTxt(address, 5),
     },
   ];
 
-  let key = 0;
   return (
-    <div>
+    <div style={{ maxWidth: "1000px", width: "100%", padding: "15px" }}>
       <h1 style={styles.title}>💰Token Balances</h1>
       <Skeleton loading={!assets}>
         <Table
           dataSource={assets}
           columns={columns}
           rowKey={(record) => {
-            key++;
-            return `${record.transaction_hash}-${key}`;
+            return record.token_address;
           }}
         />
       </Skeleton>
     </div>
   );
 }
-
 export default ERC20Balance;
